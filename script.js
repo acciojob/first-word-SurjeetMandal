35d4ba5a6ab2c86665ee2c5fc633ce2f322ad4a9
SurjeetMandal <surjeetmandal698@gmail.com>
2024-04-06 09:15:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	let res = s.trim();
+
+	let space = res.indexOf(" ");
+
+	return space !== -1 ? res.substring(0,space) : s;
 }
 
 // Do not change the code below
